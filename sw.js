ff615cf8ff2e089429d89566b2b753470b6ddbae
sakshingp <sakshi.nagpal12@gmail.com>
2019-07-10 23:50:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-importScripts("https://cdn.pushalert.co/sw-12664_2.js");
+// importScripts("https://cdn.pushalert.co/sw-12664_2.js");
+var version = 6;
+importScripts('https://cdn.pushcrew.com/sw/24bea2fb1747789fe7b14a87445f9628.js');
