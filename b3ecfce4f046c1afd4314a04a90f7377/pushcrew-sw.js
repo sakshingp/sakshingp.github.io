42fe085be362d248794cffe4e2bd524b91850951
sakshingp <sakshi.nagpal12@gmail.com>
2019-08-23 17:56:14 +0530

    Update and rename f3fea9658d703884c10feda541a35feb/pushcrew-sw.js to b3ecfce4f046c1afd4314a04a90f7377/pushcrew-sw.js

---
+++ b3ecfce4f046c1afd4314a04a90f7377/pushcrew-sw.js
@@ -0,0 +1,2 @@
+var version = 6;
+importScripts('https://cdn.pushcrewstaging.com/sw/b3ecfce4f046c1afd4314a04a90f7377.js');
--- f3fea9658d703884c10feda541a35feb/pushcrew-sw.js
+++
@@ -1,2 +0,0 @@
-var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/f3fea9658d703884c10feda541a35feb.js');
