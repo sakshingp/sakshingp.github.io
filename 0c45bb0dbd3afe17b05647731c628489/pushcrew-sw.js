f99fd2af15b833b563b7f63a5e911bba321d7b5d
sakshingp <sakshi.nagpal12@gmail.com>
2020-06-16 11:39:06 +0530

    Update and rename 0c45bb0dbd3afe17b05647731c628489/pushcrew-sw.js to 714c45373b5d8517e55307092687cf10/pushcrew-sw.js

--- 0c45bb0dbd3afe17b05647731c628489/pushcrew-sw.js
+++
@@ -1,2 +0,0 @@
-var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/0c45bb0dbd3afe17b05647731c628489.js');
---
+++ 714c45373b5d8517e55307092687cf10/pushcrew-sw.js
@@ -0,0 +1,3 @@
+var version = 6;
+importScripts('https://cdn1.pushcrew.com/sw/714c45373b5d8517e55307092687cf10.js');
+
