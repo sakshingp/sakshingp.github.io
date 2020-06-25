3a6dc10aba103fdec67aad4e61901c1f267e370f
sakshingp <sakshi.nagpal12@gmail.com>
2020-06-25 17:30:08 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn1.pushcrew.com/sw/78be0ff190b0199ed70d04833b2f346d.js');
+importScripts('https://cdn1.pushcrew.com/sw/714c45373b5d8517e55307092687cf10.js');
