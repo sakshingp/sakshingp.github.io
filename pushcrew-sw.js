2f7c3253f1456254e3960f68df9f8cae5e5e8a3d
sakshingp <sakshi.nagpal12@gmail.com>
2021-03-23 15:26:40 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,3 +1,7 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/24bea2fb1747789fe7b14a87445f9628.js');
 
+if (location.href === 'https://sakshingp.github.io/subacc.html?hash=36bedb86ef074f743dd1d1cc06d77f09') {
+  importScripts('https://cdn.pushcrew.com/sw/36bedb86ef074f743dd1d1cc06d77f09.js');
+} else if (location.href === 'https://sakshingp.github.io/pushcrew.html?hash=24bea2fb1747789fe7b14a87445f9628') {
+  importScripts('https://cdn.pushcrew.com/sw/24bea2fb1747789fe7b14a87445f9628.js');
+}
